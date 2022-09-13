22cfb1f60b24c9bb919d752d90dd7a8f57964824
nirbhay kaushik <40763483+Nirbhay91@users.noreply.github.com>
2022-09-13 09:20:18 +0530

    Update Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,29 @@
+// import React, { useEffect, useState } from "react";
+
+// export default function Board(props) {
+
+//     return (<></>)
+// }
+
 import React, { useEffect, useState } from "react";
 
 export default function Board(props) {
-
-    return (<></>)
+  const array = new Array(props.GridSize);
+  array.fill(0);
+  return (
+    <>
+      <div className="board">
+        {array.map(() => {
+          return (
+            <div className="board-row">
+              {array.map((index) => {
+                return <div key={index} className="board-box"></div>;
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
+
